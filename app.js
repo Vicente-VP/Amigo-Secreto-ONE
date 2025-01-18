9e2c255ca7120000d654f5e24fe604cbcce2b230
Vicente-VP <vvenancio06@gmail.com>
2025-01-18 18:22:26 -0300

    Desafio Concluido

---
+++ app.js
@@ -0,0 +1,37 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+let lista = document.getElementById('listaAmigos');
+
+function adicionarAmigo(){
+    let nome = document.getElementById('amigo').value;
+    if(nome == null){
+        alert('Digite o nome de um amigo');
+    }else{
+        amigos.push(nome);
+        atualizarLista(nome);
+    }
+}
+
+function atualizarLista(nome){
+    // participantes.forEach(element => {
+    //     let bullet = document.createElement('li');
+    //     bullet.appendChild(element);
+    //     lista.appendChild(bullet);
+    // });
+    let bullet = document.createElement('li');
+    let texto = document.createTextNode(nome);
+    bullet.appendChild(texto);
+    for(let i = 0; i < amigos.length; i++){
+        lista.appendChild(bullet);
+    }
+}
+
+function sortearAmigo(){
+    if(amigos.length == 0 || amigos.length == 1){
+        alert("Adicione pelo menos 2 amigos para jogar");
+    }else{
+        let resultado = document.getElementById('resultado')
+        lista.innerHTML = "";
+        resultado.innerHTML = `Seu amigo secreto é ${amigos[Math.floor(Math.random() * amigos.length)]}`;
+    }
+}
